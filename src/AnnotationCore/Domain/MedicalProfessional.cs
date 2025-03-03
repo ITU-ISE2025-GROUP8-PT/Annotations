92b5116c6b06f4383e823e81aa8a518c2edce8e2
iast <iast@itu.dk>
2025-03-03 15:34:19 +0100

    Identity user implemented + initial domain objects for users created
    Initial folder structure created to support domain entities and identity
    
    Co-authored-by: Sophie Gabriela Laksø <slak@itu.dk>

---
+++ src/AnnotationCore/Domain/MedicalProfessional.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class MedicalProfessional : User 
+{
+    public required string Affiliation  { get; set; } // the place where the medical professional works
+    public required string JobTitle  { get; set; }
+    public required int TotalAssignmentsFinished  { get; set; }
+    public required int ProfilePictureID  { get; set; }
+    
+}

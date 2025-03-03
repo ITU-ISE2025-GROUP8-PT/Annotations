a8b747a2b64d226326ce5d5757e0b42ee9aeba65
Jacob <spon@itu.dk>
2025-03-03 15:37:08 +0100

    Added UserData as API. Right now it is mocked, but will eventually be real userdata
    Co-authored-by: Nickie <nalm@itu.dk>

---
+++ Annotations.API/Controllers/UserDataController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Annotations.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserDataController : ControllerBase
+    {
+        private static readonly string[] UserNames =
+        [
+            "Sophie", "Ida", "Theresia", "Jakob", "Jacob ", "Ronas", "Nickie", "Radmehr", "Giorgi", "Daniel"
+        ];
+        
+        private static readonly string[] Occupation =
+        [
+            "Medical Professional", "Administrator"
+        ];
+
+        private readonly ILogger<UserDataController> _logger;
+
+        public UserDataController(ILogger<UserDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserData> Get(int days = 7)
+        {
+            return Enumerable.Range(1, days).Select(index => new UserData
+                (
+                    UserNames[Random.Shared.Next(UserNames.Length)],
+                    Random.Shared.Next(1, 55),
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Occupation[Random.Shared.Next(Occupation.Length)]
+                ))
+                .ToArray();
+        }
+    }
+}
---
+++ Annotations.API/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace Annotations.API;
+
+/// <summary>
+/// This is a mock class that represents users. Later this will be replaced with a real implementation.
+/// </summary>
+
+public record UserData(string? UserNames, int Age, DateOnly Date, string? Occupation)
+{
+    //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}

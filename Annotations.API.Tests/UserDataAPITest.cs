d0dfacadb349287b6cb8f4952ab15c326db58581
Sponnie <spon@itu.dk>
2025-03-03 19:32:54 +0100

    Startet to create unit test for API. Stilling missing assert
    Co-authored-by: Jakob <jakso@itu.dk>

--- Annotations.API.Tests/UserDataAPITest.cs
+++ Annotations.API.Tests/UserDataAPITest.cs
@@ -1,6 +1,19 @@
-﻿namespace DefaultNamespace;
+﻿using Xunit;
+
+namespace DefaultNamespace;
 
 public class UserDataAPITest
 {
+    [Fact]
+    public userTest()
+    {
+        //arrange
+        String firstName = "Mester";
+        String lastName = "Jakob";
+        //act
+        String lyrics = firstName + " " + lastName + " Sover du? Sover du?";
+        //assert
+        
+    }
     
 }

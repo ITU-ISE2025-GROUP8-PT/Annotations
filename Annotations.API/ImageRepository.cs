66af6198283e89a072807a97aaa116effd3afa54
Theresia <theresia.ravn@gmail.com>
2025-03-04 17:06:45 +0100

    added a skeleton to image repository

--- Annotations.API/ImageRepository.cs
+++ Annotations.API/ImageRepository.cs
@@ -1,6 +1,31 @@
 namespace Annotations.API;
+using System.IO;
 
 public class ImageRepository
 {
+    public string ProcessFile(string fileName)
+    {
+        //validate file
+        return "null";
+    }
+
+    public string UploadImage(string filepath)
+    {
+        ProcessFile(filepath);
+        //add file to db
+        return "null";
+    }
+
+    public string DownloadImage(string filepath)
+    {
+        //retrieve image form db
+        return "null";
+    }
+
+    public string DeleteImage(string filepath)
+    {
+        return "null";
+    }
+    
     
 }

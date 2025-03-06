780cc4720e16e0c893760833f62494b6f30fce58
iast <iast@itu.dk>
2025-03-06 13:51:41 +0100

    Added validation to file processing
    Co-authored-by: Theresia Ravn <theresia.ravn@gmail.com>

--- Annotations.API/ImageRepository.cs
+++ Annotations.API/ImageRepository.cs
@@ -3,13 +3,27 @@ using System.IO;
 
 public class ImageRepository
 {
-    public string ProcessFile(string fileName)
+    public async Task<> ProcessFile(IFormFile file)
     {
-        //validate file
-        return "null";
+        if (file.Length > 5 * 1024 * 1024)
+        {
+            return BadRequest("File size is too big");
+        }
+        else if (if file == null || file.Length == 0)
+        {
+             return BadRequest("File doen't exist");
+        }
+        else if (file.ContentType != "image/jpeg" || file.ContentType != "image/png")
+        {
+            return BadRequest("File is not a jpeg or png");
+        }
+        else
+        {
+            return Ok("File validated")
+        }
     }
 
-    public string UploadImage(string filepath)
+    public string UploadImage(IFormFile filepath)
     {
         ProcessFile(filepath);
         //add file to db

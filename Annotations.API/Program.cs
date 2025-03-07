55a437bc9f2e6148286dcaab1aaf49220ec9e6bc
Sponnie <spon@itu.dk>
2025-03-07 20:44:23 +0100

    Very good bare bone version of minimal API with errorhandling!
    Co-authored-by: Ronas Olsen <rono@itu.dk>

--- Annotations.API/Program.cs
+++ Annotations.API/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddDbContext<AnnotationsDbContext>((serviceProvider, options) =
 var app = builder.Build();
 
 var usersMapRoute = app.MapGroup("/users");
+app.MapGet("/error", () => "Dette er en 400-599 eller værre");
 
 UsersGroup.MapEndpoints(usersMapRoute);
 
@@ -42,6 +43,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // Middleware pipeline.
+app.UseExceptionHandler("/error");
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();

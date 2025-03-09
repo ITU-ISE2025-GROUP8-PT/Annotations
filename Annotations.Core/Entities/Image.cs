ccb1e2e9144c3393609655305cc877dc78475646
Sponnie <spon@itu.dk>
2025-03-09 18:23:56 +0100

    Made quickfix for build failure
    Co-authored-by: Ronas Olsen <rono@itu.dk>

--- Annotations.Core/Entities/Image.cs
+++ Annotations.Core/Entities/Image.cs
@@ -8,6 +8,11 @@ public class Image
     [StringLength(100)]
     public required string Title { get; set; }
     [StringLength(1000)]
-    public required string Text { get; set; }
+    public required string Description { get; set; }
     public required byte[] ImageData { get; set; }
 }
+
+    /*Later add foreign keys:
+    UploadedBy (referencing AdminId)
+    DeletedBy (referencing AdminId)
+    */

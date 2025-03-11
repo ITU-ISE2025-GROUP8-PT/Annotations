c901618705635568ca8a57e9683feeeba1d65d68
Theresia <theresia.ravn@gmail.com>
2025-03-11 18:40:55 +0100

    Initial validation for image upload
    Co-authored-by: Nickie Almind <nalm@itu.dk>

--- Annotations.API/Groups/ImagesGroup.cs
+++ Annotations.API/Groups/ImagesGroup.cs
@@ -1,4 +1,5 @@
 ﻿using Annotations.Core.Models;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,35 @@ public static class ImagesGroup
     public static void MapEndpoints(RouteGroupBuilder pathBuilder)
     {
         pathBuilder.MapGet("/", () => "Upload an image here!");
+
+        pathBuilder.MapPost("/images", async (HttpRequest request) =>
+        {
+            IFormFile file = request.Form.Files[0];
+            if (file.ContentType != "image/jpeg" || file.ContentType != "image/png")
+            {
+                return Results.BadRequest("File is not a valid image.");
+            } else if (file.Length > 5 * 1024 * 1024)
+            {
+                return Results.BadRequest("File is too large.");
+            }
+            else if (file.Length == 0) 
+            { 
+                Results.BadRequest("File doesn't exist.");
+            }
+            else
+            {
+                //upload image to db
+                return Results.Ok("Uploaded image successfully.");
+            }
+            return Results.BadRequest(string.Empty);
+
+            //var url = request.GetDisplayUrl();
+
+            //var path = request.Path;
+            
+
+        });
+        
         //Husk: Delete an image (kun image ID), Get an image by imageId (kun image ID), Update / PATCH an image (title og text) fra openapi dokumentet
         
         // hvis JSON objektet skal returneres brug da:

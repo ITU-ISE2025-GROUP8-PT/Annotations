5ac2fe008a37726c392512aac64e65b97ccace6b
Ronas Olsen <rono@itu.dk>
2025-03-12 11:48:15 +0100

    Run on local sqlserver.

--- Annotations.API/Program.cs
+++ Annotations.API/Program.cs
@@ -13,22 +13,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-// Temporary SQLite based database service.
-// This singleton pattern allows in-memory SQLite to work correctly.
-// From: https://www.answeroverflow.com/m/1071789602316238919
-builder.Services.AddSingleton(_ =>
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<AnnotationsDbContext>(options => 
 {
-    var connection = new SqliteConnection("Data Source=:memory:");
-    connection.Open();
-    return connection;
+    options.UseSqlServer(connectionString);
 });
-
-builder.Services.AddDbContext<AnnotationsDbContext>((serviceProvider, options) =>
-{
-    var connection = serviceProvider.GetRequiredService<SqliteConnection>();
-    options.UseSqlite(connection);
-});
-
 
 var app = builder.Build();
 
@@ -47,7 +36,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    InitializeTempDatabase();
 }
 
 // Middleware pipeline.
@@ -60,43 +48,6 @@ app.UseHttpsRedirection();
 app.Run();
 
 
-
-
-// Helper to initialize a database within developer environment.
-void InitializeTempDatabase()
-{
-    using var scope = app.Services.CreateScope();
-    using var context = scope.ServiceProvider.GetRequiredService<AnnotationsDbContext>();
-    context.Database.Migrate();
-
-    context.Add(new Admin
-    {
-        UserId = 0,
-        FirstName = "Admin",
-        LastName = "Adminsen",
-        Email = "admin@adminsen.com"
-    }); 
-    context.Add(new MedicalProfessional
-    {
-        UserId = 1,
-        FirstName = "Medical",
-        LastName = "Professional",
-        Email = "med@prof.com",
-        Affiliation = "Rigshospitalet",
-        JobTitle = "Surgeon",
-        TotalAssignmentsFinished = 0,
-        ProfilePictureId = 123
-    });
-    context.Add(new Image
-    {
-        Id = 1,
-        Title = "Sample Image",
-        Description = "This is a sample image.",
-        ImageData = File.ReadAllBytes("../docs/images/Perfusiontech_sampleimage.png")
-        // ImageData = await GetImageDataAsync("Perfusiontech_sampleimage.png"); <-- Eller hvad den nu kommer til at hedde når den smides op
-    });
-    context.SaveChanges();
-}
 
 /*
 // Helper metode til at hive billeder ud som Byte[] fra blob

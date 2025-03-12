d7da4b8da8fd96261b1fedf1ab7d9ba0d162d544
Ronas Olsen <rono@itu.dk>
2025-03-12 14:23:53 +0100

    Simplified usersgroup. Program.cs for identity.

--- Annotations.API/Program.cs
+++ Annotations.API/Program.cs
@@ -1,102 +1,69 @@
 using Annotations.API;
 using Annotations.API.Groups;
 using Annotations.Core.Entities;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 // using Azure.Storage.Blobs;
 // using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// General services.
+// Services for development using SwaggerUI.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-// Temporary SQLite based database service.
-// This singleton pattern allows in-memory SQLite to work correctly.
-// From: https://www.answeroverflow.com/m/1071789602316238919
-builder.Services.AddSingleton(_ =>
+// Database context service.
+builder.Services.AddDbContext<AnnotationsDbContext>(options =>
 {
-    var connection = new SqliteConnection("Data Source=:memory:");
-    connection.Open();
-    return connection;
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddDbContext<AnnotationsDbContext>((serviceProvider, options) =>
-{
-    var connection = serviceProvider.GetRequiredService<SqliteConnection>();
-    options.UseSqlite(connection);
-});
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<AnnotationsUser>()
+                .AddEntityFrameworkStores<AnnotationsDbContext>();
 
 
+// Build application.
 var app = builder.Build();
+
 
 // Blob Storage connection string HUSK ""
 //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString); <-- indsæt string her i enden og intet andet i linjen
 // Blob container name string HUSK ""
 //BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName); <-- indsæt string her i enden og intet andet i linjen
 
-UsersGroup.MapEndpoints(app.MapGroup("/users"));
-ImagesGroup.MapEndpoints(app.MapGroup("/images"));
+app.MapGroup("/Auth").MapIdentityApi<AnnotationsUser>();
 
-app.MapGet("/error", () => "Dette er en 400-599 eller værre");
+UsersGroup.MapEndpoints(app.MapGroup("/Users"));
+ImagesGroup.MapEndpoints(app.MapGroup("/Images"));
+
+app.MapGet("/Error", () => "Dette er en 400-599 eller værre");
 
 // Development/Debugging middleware.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    InitializeTempDatabase();
 }
 
 // Middleware pipeline.
-app.UseExceptionHandler("/error");
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
+app.UseExceptionHandler("/Error");
+
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+
+// Application start.
 app.Run();
-
-
-
-
-// Helper to initialize a database within developer environment.
-void InitializeTempDatabase()
-{
-    using var scope = app.Services.CreateScope();
-    using var context = scope.ServiceProvider.GetRequiredService<AnnotationsDbContext>();
-    context.Database.Migrate();
-
-    context.Add(new Admin
-    {
-        UserId = 0,
-        FirstName = "Admin",
-        LastName = "Adminsen",
-        Email = "admin@adminsen.com"
-    }); 
-    context.Add(new MedicalProfessional
-    {
-        UserId = 1,
-        FirstName = "Medical",
-        LastName = "Professional",
-        Email = "med@prof.com",
-        Affiliation = "Rigshospitalet",
-        JobTitle = "Surgeon",
-        TotalAssignmentsFinished = 0,
-        ProfilePictureId = 123
-    });
-    context.Add(new Image
-    {
-        Id = 1,
-        Title = "Sample Image",
-        Description = "This is a sample image.",
-        ImageData = File.ReadAllBytes("../docs/images/Perfusiontech_sampleimage.png")
-        // ImageData = await GetImageDataAsync("Perfusiontech_sampleimage.png"); <-- Eller hvad den nu kommer til at hedde når den smides op
-    });
-    context.SaveChanges();
-}
 
 /*
 // Helper metode til at hive billeder ud som Byte[] fra blob

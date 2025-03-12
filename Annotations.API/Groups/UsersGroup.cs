9f54f55bd3c266828818ef6e5405d46f5b791111
Ronas Olsen <rono@itu.dk>
2025-03-12 14:23:53 +0100

    Simplified usersgroup. Program.cs for identity.

--- Annotations.API/Groups/UsersGroup.cs
+++ Annotations.API/Groups/UsersGroup.cs
@@ -1,7 +1,3 @@
-using Annotations.Core.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
 namespace Annotations.API.Groups;
 
 public static class UsersGroup
@@ -9,36 +5,9 @@ public static class UsersGroup
     public static void MapEndpoints(RouteGroupBuilder pathBuilder)
     {
         pathBuilder.MapGet("/", () => "Hello Kitty!");
-        pathBuilder.MapGet("/admins", async (AnnotationsDbContext context) =>
-            {
-                var admins = await context.Admins
-                    .Select(u => new AdminUserModel
-                    {
-                        Id = u.UserId,
-                        FirstName = u.FirstName,
-                        LastName = u.LastName,
-                        Email = u.Email
-                    })
-                    .ToListAsync();
-                return admins;
-            });
-        pathBuilder.MapGet("/medicalprofessionals", async (AnnotationsDbContext context) =>
-            {
-                var medicalProfessionals = await context.MedicalProfessionals
-                    .Select(u => new MedicalProfessionalUserModel
-                    {
-                        Id = u.UserId,
-                        FirstName = u.FirstName,
-                        LastName = u.LastName,
-                        Email = u.Email,
-                        Affiliation = u.Affiliation,
-                        JobTitle = u.JobTitle,
-                        TotalAssignmentsFinished = u.TotalAssignmentsFinished,
-                        ProfilePictureID = u.ProfilePictureId
-                    })
-                    .ToListAsync();
-                return medicalProfessionals;
-            });
+
+        pathBuilder.MapGet("/ShowAll", () => "Access Granted!").RequireAuthorization();
+
         pathBuilder.MapGet("/exception", () => 
         {
             throw new InvalidOperationException("Exception has been raised in the API. Look for further details in the log.");

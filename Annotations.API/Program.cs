595c7c399240efcdaf6480be91d79f4bc37fcf41
Sponnie <spon@itu.dk>
2025-03-14 22:22:09 +0100

    Sponnies local test branch

--- Annotations.API/Program.cs
+++ Annotations.API/Program.cs
@@ -3,7 +3,7 @@ using Annotations.API.Groups;
 using Annotations.Core.Entities;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-// using Azure.Storage.Blobs;
+using Azure.Storage.Blobs;
 // using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,6 +36,35 @@ var app = builder.Build();
 //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString); <-- indsæt string her i enden og intet andet i linjen
 // Blob container name string HUSK ""
 //BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName); <-- indsæt string her i enden og intet andet i linjen
+//var connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02x...;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
+var connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
+
+// Test blob .txt upload
+BlobClient blobClient = containerClient.GetBlobClient("test.txt");
+await blobClient.UploadAsync(
+    new MemoryStream("Hello, Azurite!"u8.ToArray()), 
+    overwrite: true
+);
+Console.WriteLine("Uploaded and potentially overwrite 'test.txt' to Azurite.");
+
+// Test blob .txt download
+var memoryStream = new MemoryStream();
+await blobClient.DownloadToAsync(memoryStream);
+string content = System.Text.Encoding.UTF8.GetString(memoryStream.ToArray());
+Console.WriteLine($"Downloaded content: {content}");
+
+// Test blob image file upload
+var imagePath = "../docs/images/Perfusiontech_sampleimage.png";
+BlobClient imageBlobClient = containerClient.GetBlobClient("Perfusiontech_sampleimage.png");
+using (var fileStream = File.OpenRead(imagePath))
+{
+    await imageBlobClient.UploadAsync(fileStream, overwrite: true);
+    Console.WriteLine("Uploaded 'Perfusiontech_sampleimage.png' to Azurite.");
+}
+
+
 
 UsersGroup.MapEndpoints(app.MapGroup("/users"));
 ImagesGroup.MapEndpoints(app.MapGroup("/images"));

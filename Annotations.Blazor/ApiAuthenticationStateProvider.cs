ecdeb5aaf65d7046a7ef4fb4af0f4fa8cf868cbf
Jakob <jakso@itu.dk>
2025-03-15 15:11:39 +0100

    Added a custom ApiAuthenticationStateProvider to access JWT tokens from API

---
+++ Annotations.Blazor/ApiAuthenticationStateProvider.cs
@@ -0,0 +1,113 @@
+/*
+ * https://chrissainty.com/securing-your-blazor-apps-authentication-with-clientside-blazor-using-webapi-aspnet-core-identity/
+ */
+
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Annotations.Blazor;
+
+public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorage;
+
+    public ApiAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+    {
+        _httpClient = httpClient;
+        _localStorage = localStorage;
+    }
+    
+    /*
+     * The GetAuthenticationStateAsync method is called by the CascadingAuthenticationState component,
+     * to determine if the current user is authenticated or not.
+     */
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var savedToken = await _localStorage.GetItemAsync<string>("authToken");
+
+        if (string.IsNullOrWhiteSpace(savedToken))
+        {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())); // User is not authenticated
+        }
+
+        // If there is a token, we retrieve it and set the default authorization header for the HttpClient
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
+
+        // new claims principal containing the claims from the token
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+    }
+
+    /*
+     * Helper method used when a user logs in.
+     * Its sole purpose is to invoke the NotifyAuthenticationStateChanged method,
+     * which fires an event called AuthenticationStateChanged.
+     * This cascades the new authentication state, via the CascadingAuthenticationState component.
+     */
+    public void MarkUserAsAuthenticated(string email)
+    {
+        var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
+        var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    /*
+     * Same principle as MethodMarkUserAsAuthenticated, but for logging out.
+     */
+    public void MarkUserAsLoggedOut()
+    {
+        var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+        var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    /*
+     * Method from  Steve Sandersons Mission Control demo app: https://github.com/SteveSandersonMS/presentation-2019-06-NDCOslo/tree/master/demos/MissionControl
+     * This method decodes the token and returns the claims contained within it.
+     */
+    private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var claims = new List<Claim>();
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = ParseBase64WithoutPadding(payload);
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+        keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+        if (roles != null)
+        {
+            if (roles.ToString().Trim().StartsWith("["))
+            {
+                var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+
+                foreach (var parsedRole in parsedRoles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                }
+            }
+            else
+            {
+                claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+            }
+
+            keyValuePairs.Remove(ClaimTypes.Role);
+        }
+
+        claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+
+        return claims;
+    }
+
+    private byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+        return Convert.FromBase64String(base64);
+    }
+}

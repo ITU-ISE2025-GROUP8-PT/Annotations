7f366c6f7b5a359c9c46d44c21881ee59006cbae
nalm2812 <nalm@itu.dk>
2025-03-15 13:40:46 +0100

    validating image works functionality-wise
    Co-authored-by: Theresia Ravn <theresia.ravn@gmail.com>
    Co-authored-by: Jacob Sponholtz <spon@itu.dk>
    Co-authored-by: Ronas Olsen <rono@itu.dk>

--- Annotations.API/Groups/ImagesGroup.cs
+++ Annotations.API/Groups/ImagesGroup.cs
@@ -4,42 +4,38 @@ using Annotations.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Annotations.API.Groups;
 
 public static class ImagesGroup
 {
+    private static int counter = 0;//change this - it gets reset every time the program resets
+    private record ValidationResponse(bool Success, string ErrorMessage);
+    private static ValidationResponse ValidateImage(IFormFile file)//temporary location
+    {
+        Console.WriteLine("validating image");
+        if (file.ContentType != "image/jpeg" && file.ContentType != "image/png" &&  file.ContentType != "image/jpg")
+        {
+            Console.WriteLine("Invalid image format");
+            return new ValidationResponse(false, "File is not a valid image.");
+        } 
+        if (file.Length > 5 * 1024 * 1024) {
+            return new ValidationResponse(false, "File is too large.");
+        }
+        if (file.Length == 0) 
+        { 
+            return new ValidationResponse(false, "File doesn't exist.");
+        }
+        //upload image to db
+        return new ValidationResponse(true ,"Uploaded image successfully.");
+        
+    }
     public static void MapEndpoints(RouteGroupBuilder pathBuilder)
     {
         pathBuilder.MapGet("/", () => "Upload an image here!");
 
-        pathBuilder.MapPost("/imagess", async (HttpRequest request) =>
-        {
-            IFormFile file = request.Form.Files[0];
-            if (file.ContentType != "image/jpeg" || file.ContentType != "image/png")
-            {
-                return Results.BadRequest("File is not a valid image.");
-            } else if (file.Length > 5 * 1024 * 1024)
-            {
-                return Results.BadRequest("File is too large.");
-            }
-            else if (file.Length == 0) 
-            { 
-                Results.BadRequest("File doesn't exist.");
-            }
-            else
-            {
-                //upload image to db
-                return Results.Ok("Uploaded image successfully.");
-            }
-            return Results.BadRequest(string.Empty);
-
-            //var url = request.GetDisplayUrl();
-
-            //var path = request.Path;
-            
-
-        });
+       
         
         //Husk: Delete an image (kun image ID), Get an image by imageId (kun image ID), Update / PATCH an image (title og text) fra openapi dokumentet
         
@@ -51,34 +47,32 @@ public static class ImagesGroup
             return image is not null ? Results.Ok(image) : Results.NotFound();
         });
         */
-        pathBuilder.MapPost("/upload", async (IFormFile image, AnnotationsDbContext context) =>
+        pathBuilder.MapPost("/upload", async (IFormFile image) =>
         {
-            /*using HttpClient client = new HttpClient(); 
-            Uri uri = new Uri(url);*/
-            
-            var connectionString =
-                "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
-            
-            
-            BlobClient imageBlobClient = containerClient.GetBlobClient("idk");
-            using (var fileStream = image.OpenReadStream())
+          
+            ValidationResponse response = ValidateImage(image);
+            if (!response.Success)
             {
-                await imageBlobClient.UploadAsync(fileStream, overwrite: true);
-                Console.WriteLine("Uploaded image successfully.");
+                Console.WriteLine("rejecting image");
+                //TODO: how should the end user see this?
             }
-            
-            /*var image = new Image
+            else
             {
-                Id = 03,
-                Title = "idk3",
-                Description = "idk4",
-                ImageData = response
-            };
-            await context.Images.AddAsync(image);
-            await context.SaveChangesAsync();
-            return Results.Ok(new { message = "Image uploaded successfully" });*/
+                Console.WriteLine("Image uploading.");
+                var connectionString =
+                    "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+                BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
+            
+            
+                BlobClient imageBlobClient = containerClient.GetBlobClient($"{counter}.jpg");
+                counter++;
+                using (var fileStream = image.OpenReadStream())
+                {
+                    await imageBlobClient.UploadAsync(fileStream, overwrite: true);
+                    Console.WriteLine("Uploaded image successfully.");
+                }
+            }
 
 
         }).DisableAntiforgery();

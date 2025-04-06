d8237f1c8c3df48489ba7659515b80a68e8a860b
Sponnie <spon@itu.dk>
2025-04-06 15:40:18 +0200

    Test for image on Annotation Tool is added
    Co-authored-by: Jakob <jakso@itu.dk>

--- Annotations.Blazor.Tests/Annotations.Blazor.Tests.Unit/AnnotateOnImageTests.cs
+++ Annotations.Blazor.Tests/Annotations.Blazor.Tests.Unit/AnnotateOnImageTests.cs
@@ -19,4 +19,18 @@ public class AnnotateOnImageTests : TestContext
         annotatePage.Find("h1").MarkupMatches("<h1>Annotations - PerfusionTech</h1>");
         annotatePage.Find("p").MarkupMatches("<p> Log in or make a new account to get access</p>");
     }
+    
+    [Fact]
+    public void AuthorizedTest()
+    {
+        // Arrange
+        JSInterop.Mode = JSRuntimeMode.Loose; // allows JS to run during bUnit tests
+        var authContext = this.AddTestAuthorization();
+        authContext.SetAuthorized("FAKE USER TEST"); // fakes authorized state for a fake user
+        var annotatePage = RenderComponent<AnnotateOnImage>();
+        
+        // Assert
+        annotatePage.Find("img").MarkupMatches("<img src=\"img/billede1.png\" alt=\"Profile Picture\" style=\"width: 80px; height: 80px; border-radius: 50%; object-fit: cover;\">");
+        
+    }
 }

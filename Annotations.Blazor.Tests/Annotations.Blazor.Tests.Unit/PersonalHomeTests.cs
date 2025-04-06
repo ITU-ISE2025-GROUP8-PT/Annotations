2e104066737e08b02dc5a7ce330c1bf745fd30de
Jakob <jakso@itu.dk>
2025-04-06 14:59:31 +0200

    First unit test up and running!!
    Co-authored-by: Jacob Sponholtz <spon@itu.dk>

---
+++ Annotations.Blazor.Tests/Annotations.Blazor.Tests.Unit/PersonalHomeTests.cs
@@ -0,0 +1,20 @@
+using Annotations.Blazor.Components.Pages;
+
+namespace Annotations.Blazor.Tests.Annotations.Blazor.Tests.Unit;
+
+public class PersonalHomeTests : TestContext
+{
+    [Fact]
+    public void NotAuthorizedTest()
+    {
+        // Arrange
+        JSInterop.Mode = JSRuntimeMode.Loose; // allows JS to run during bUnit tests
+        var authContext = this.AddTestAuthorization();
+        authContext.SetAuthorized("", AuthorizationState.Unauthorized); // fakes unauthorized state for a fake user
+        var homePage = RenderComponent<Home>();
+        
+        // Assert
+        homePage.Find("h1").MarkupMatches("<h1>Annotations - PerfusionTech</h1>");
+        homePage.Find("p").MarkupMatches("<p> Log in or make a new account to get access</p>");
+    }
+}

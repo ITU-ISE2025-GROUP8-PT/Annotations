c78fb776fe4ddd048b53878679fff46172f78913
Jakob <jakso@itu.dk>
2025-04-06 15:02:28 +0200

    authorized user home page test.
    Co-authored-by: Jacob <spon@itu.dk>

--- Annotations.Blazor.Tests/Annotations.Blazor.Tests.Unit/PersonalHomeTests.cs
+++ Annotations.Blazor.Tests/Annotations.Blazor.Tests.Unit/PersonalHomeTests.cs
@@ -17,4 +17,17 @@ public class PersonalHomeTests : TestContext
         homePage.Find("h1").MarkupMatches("<h1>Annotations - PerfusionTech</h1>");
         homePage.Find("p").MarkupMatches("<p> Log in or make a new account to get access</p>");
     }
+    
+    [Fact]
+    public void AuthorizedTest()
+    {
+        // Arrange
+        JSInterop.Mode = JSRuntimeMode.Loose; // allows JS to run during bUnit tests
+        var authContext = this.AddTestAuthorization();
+        authContext.SetAuthorized("FAKE TEST USER"); // fakes authorized state for a fake user
+        var homePage = RenderComponent<Home>();
+        
+        // Assert
+        homePage.Find("h2").MarkupMatches("<h2> Welcome user</h2>");
+    }
 }

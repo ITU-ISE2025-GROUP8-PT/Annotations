2ce0bb90badb95909e59630b18eda9570b3a2979
Sponnie <spon@itu.dk>
2025-04-06 15:28:57 +0200

    Authorized AnnotationOnImage endpoint. If not authorized redirect to homepage instead.
    Co-authored-by: Jakob <jakso@itu.dk>

---
+++ Annotations.Blazor.Tests/Annotations.Blazor.Tests.Unit/AnnotateOnImageTests.cs
@@ -0,0 +1,17 @@
+﻿using Annotations.Blazor.Components.Pages;
+
+namespace Annotations.Blazor.Tests.Annotations.Blazor.Tests.Unit;
+
+public class AnnotateOnImageTests : TestContext
+{
+
+    [Fact]
+    public void NotAuthorizedTest()
+    {
+        // Arrange
+        JSInterop.Mode = JSRuntimeMode.Loose; // allows JS to run during bUnit tests
+        var authContext = this.AddTestAuthorization();
+        authContext.SetAuthorized("FAKE TEST USER"); // fakes authorized state for a fake user
+        var homePage = RenderComponent<Home>();
+    }
+}

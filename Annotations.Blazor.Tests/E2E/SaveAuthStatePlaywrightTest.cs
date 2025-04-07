9d37a706332f69d643d74fc961ff90c29eff7203
Jakob <jakso@itu.dk>
2025-04-07 11:25:16 +0200

    small changes in code, removal of comments

--- Annotations.Blazor.Tests/E2E/SaveAuthStatePlaywrightTest.cs
+++ Annotations.Blazor.Tests/E2E/SaveAuthStatePlaywrightTest.cs
@@ -9,25 +9,21 @@ public class SaveAuthStatePlaywrightTest
     [Fact]
     public async Task SaveAuthenticatedState()
     {
-        string[] loginCred = File.ReadAllLines("../../../../playwright/.auth/testUser.txt");
+        string[] loginCred = await File.ReadAllLinesAsync("../../../../playwright/.auth/testUser.txt");
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync();
         var context = await browser.NewContextAsync();
         var page = await context.NewPageAsync();
 
-        // Navigate to Orchard Core login page (replace with your URL)
         await page.GotoAsync("https://localhost:7238/authentication/login");
 
-        // Fill Orchard Core credentials (adjust selectors as needed)
         await page.GetByLabel("Username or email address").FillAsync(loginCred[0]);
         
         await page.GetByLabel("Password").FillAsync(loginCred[1]);
-        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-
-        // Wait for post-login redirect (optional)
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Log in" }).ClickAsync();
+        
         await page.WaitForURLAsync("https://localhost:7238/");
 
-        // Save state to a file (creates playwright/.auth/state.json)
         await context.StorageStateAsync(new()
         {
             Path = "../../../../playwright/.auth/orchard-state.json"

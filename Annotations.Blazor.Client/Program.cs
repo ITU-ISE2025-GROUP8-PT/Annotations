388f2c3c95883bd59a4ea9e5068692dcace6ef69
Ronas Olsen <rono@itu.dk>
2025-04-09 16:44:26 +0200

    Added web-assembly client project.
    Initial code from bff sample project included.

---
+++ Annotations.Blazor.Client/Program.cs
@@ -0,0 +1,23 @@
+/*
+ * The following code is based on https://github.com/dotnet/blazor-samples and https://github.com/dotnet/blazor-samples/tree/main/8.0/BlazorWebAppOidcBff
+ * Provided by Microsoft Corporation under the MIT license.
+ */
+
+using Annotations.Blazor.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+/*
+builder.Services.AddHttpClient<IWeatherForecaster, ClientWeatherForecaster>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+*/
+
+await builder.Build().RunAsync();

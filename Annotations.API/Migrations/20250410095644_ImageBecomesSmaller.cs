f2eec473d38248a0261d324852764b42a0b39c2d
nalm2812 <nalm@itu.dk>
2025-04-10 12:05:19 +0200

    actually pushing migrations

---
+++ Annotations.API/Migrations/20250410095644_ImageBecomesSmaller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Annotations.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageBecomesSmaller : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageData",
+                table: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageData",
+                table: "Images",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}

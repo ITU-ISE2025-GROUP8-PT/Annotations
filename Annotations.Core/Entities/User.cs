e26eff1c3e049832aceb7ea202708103912ac817
Ronas Olsen <rono@itu.dk>
2025-04-19 15:22:18 +0200

    User entity simplified.
    Details on how to use and important considerations included.

--- Annotations.Core/Entities/User.cs
+++ Annotations.Core/Entities/User.cs
@@ -4,14 +4,20 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Annotations.Core.Entities;
 
-public class User : IdentityUser
+/// <summary>
+/// <para>User entity for the purpose of storing user information for app specific purposes.</para>
+/// </summary>
+public class User
 {
+    /*
+     * Attention: 
+     * In general, unless the SSO is known to impose string length limits,
+     * do not try to impose those limits here. Essential properties like 
+     * UserId, UserName, Email, Phone, etc. should match SSO provider. 
+     */ 
     [Key]
-    public required int UserId {get; set; }
+    public required string UserId {get; set; } // User ID matching that given by SSO provider. 
+
     [Required]
-    [StringLength(50)]
-    public required string FirstName { get; set; }
-    [Required]
-    [StringLength(50)]
-    public required string LastName { get; set; }
+    public required string UserName { get; set; } // User name for display purposes. 
 }

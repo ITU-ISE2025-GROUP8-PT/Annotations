55218935ccac4c6767eb80ba2fc43179c15e5ec1
Ronas Olsen <rono@itu.dk>
2025-04-19 16:02:18 +0200

    Added docs for existing methods.

--- Annotations.API/Images/ImageEndpoints.cs
+++ Annotations.API/Images/ImageEndpoints.cs
@@ -6,12 +6,19 @@ namespace Annotations.API.Images;
 
 public class ImageEndpoints
 {
+    /// <summary>
+    /// Maps all application endpoints for image upload and download. 
+    /// </summary>
+    /// <param name="groupBuilder"></param>
     public static void MapEndpoints(RouteGroupBuilder groupBuilder)
     {
-        groupBuilder.DisableAntiforgery();
+        groupBuilder.DisableAntiforgery(); // Disabled as this is an API, which will not serve any forms.
         groupBuilder.MapPost("/upload", ImagesUploadDelegate).RequireAuthorization();
     }
 
+    /// <summary>
+    /// <para>Delegate for <c>/images/upload</c> POST request.</para>
+    /// </summary>
     private static Delegate ImagesUploadDelegate => async (
         IFormFile       image,
         string          series,

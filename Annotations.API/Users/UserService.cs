ddd615883fc9bccf1e3c78ecd1c3f4591fa00707
Ronas Olsen <rono@itu.dk>
2025-04-21 15:07:24 +0200

    Doc for UserService.cs

--- Annotations.API/Users/UserService.cs
+++ Annotations.API/Users/UserService.cs
@@ -3,9 +3,24 @@ using System.Security.Claims;
 
 namespace Annotations.API.Users;
 
+/// <summary>
+/// Defines a scoped service for user information stored in the backend API database.
+/// </summary>
 public interface IUserService
 {
+    /// <summary>
+    /// <para>Finds the users own database entry by their claims principal.</para>
+    /// <para>Returns <c>null</c> if user is not recorded in the database.</para>
+    /// </summary>
+    /// <param name="claimsPrincipal">Claims principal from authenticated http context.</param>
     Task<User?> TryFindUserAsync(ClaimsPrincipal claimsPrincipal);
+
+    /// <summary>
+    /// <para>Creates a database entry for the user based on their claims principal.</para>
+    /// <para>An <c>InvalidOperationException</c> is thrown if user is already recorded in the database.</para>
+    /// </summary>
+    /// <param name="claimsPrincipal">Claims principal from authenticated http context.</param>
+    /// <return>The newly created database entry for the user.</return>
     Task<User> CreateUser(ClaimsPrincipal claimsPrincipal);
 }
 
@@ -20,6 +35,11 @@ public class UserService : IUserService
 
     public async Task<User> CreateUser(ClaimsPrincipal claimsPrincipal)
     {
+        if (await TryFindUserAsync(claimsPrincipal) != null)
+        {
+            throw new InvalidOperationException("This user already exists");
+        }
+
         var newUser = new User
         {
             UserId      = claimsPrincipal.FindFirstValue("sub") ?? throw new ArgumentNullException(nameof(claimsPrincipal)),

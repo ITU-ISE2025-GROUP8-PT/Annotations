4313576fa46ae7fba0cae700c8a1987b6cef6424
Ronas Olsen <rono@itu.dk>
2025-04-23 22:17:43 +0200

    Download image not ready.

---
+++ Annotations.Blazor/Helpers/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace Annotations.Blazor.Helpers;
+
+/// <summary>
+/// https://jasonwatmore.com/post/2020/08/09/blazor-webassembly-get-query-string-parameters-with-navigation-manager
+/// </summary>
+public static class ExtensionMethods
+{
+    // get entire querystring name/value collection
+    public static NameValueCollection QueryString(this NavigationManager navigationManager)
+    {
+        return HttpUtility.ParseQueryString(new Uri(navigationManager.Uri).Query);
+    }
+
+    // get single querystring value with specified key
+    public static string QueryString(this NavigationManager navigationManager, string key)
+    {
+        return navigationManager.QueryString()[key] ?? string.Empty;
+    }
+}

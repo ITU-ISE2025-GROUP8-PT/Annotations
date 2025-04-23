40f8efce43c39528406e989de3d6f4957dd5f37e
Ronas Olsen <rono@itu.dk>
2025-04-23 18:17:32 +0200

    Server image uploader stub.
    Co-authored-by: Jacob spon@itu.dk

---
+++ Annotations.Blazor/ImageServices/IImageUploader.cs
@@ -0,0 +1,13 @@
+﻿using MatBlazor;
+
+namespace Annotations.Blazor.ImageServices
+{
+    public interface IImageUploader
+    {
+        Task<long> CreateImageSeriesAsync(string name, string category);
+
+        Task<string> ImageSeriesAppendAsync(long imageSeriesId, string[] imageIds);
+
+        Task<string[]> UploadImagesAsync(IMatFileUploadEntry[] images);
+    }
+}
---
+++ Annotations.Blazor/ImageServices/ServerImageUploader.cs
@@ -0,0 +1,22 @@
+﻿using MatBlazor;
+
+namespace Annotations.Blazor.ImageServices
+{
+    public class ServerImageUploader : IImageUploader
+    {
+        public Task<long> CreateImageSeriesAsync(string name, string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ImageSeriesAppendAsync(long imageSeriesId, string[] imageIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string[]> UploadImagesAsync(IMatFileUploadEntry[] images)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

7a48cf21e4f3d55911bbb1b753a412fc83bdeb7a
Theresia <theresia.ravn@gmail.com>
2025-04-29 09:12:10 +0200

    documented createGetResponse
    Co-authored-by: Nickie Almind <nalm@itu.dk>
    Co-authored-by: Jacob Sponholtz <spon@itu.dk>
    Co-authored-by: Radmehr Shafaat <rads@itu.dk>

--- Annotations.Blazor/Services/APIServices.cs
+++ Annotations.Blazor/Services/APIServices.cs
@@ -18,16 +18,24 @@ public class APIServices : IAPIServices
         _clientFactory = clientFactory;
     }
 
+    /// <summary>
+    /// Gets the httpContext and thereafter pass along an accesstoken that the httpcontext has.
+    /// Calls the Web API. 
+    /// Response is received and passed along.
+    /// </summary>
+    /// <param name="requestURI"></param>
+    /// <returns>Response of the Web API</returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public async Task<HttpResponseMessage> createGetResponse(string requestURI)
     {
         var httpContext = _httpContextAccessor.HttpContext ??
-                          throw new InvalidOperationException("No HttpContext available"); //make connection
+                          throw new InvalidOperationException("No HttpContext available"); //receive httpContext
 
         var accessToken = await httpContext.GetTokenAsync("access_token") ??
                           throw new InvalidOperationException("No access_token was saved"); //authorization
 
         var client = _clientFactory.CreateClient();
-        client.BaseAddress = new("https://localhost:7250"); //connect to Web API
+        client.BaseAddress = new("https://localhost:7250"); //Web API address
 
         using var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestURI); //access endpoint
         requestMessage.Headers.Authorization = new("Bearer", accessToken); //bearer token for authorization

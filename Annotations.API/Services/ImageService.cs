04cdc7a314f9bf6d5f8f2675483f822d2d14dcde
Theresia <theresia.ravn@gmail.com>
2025-04-30 11:04:54 +0200

    moved imagevalidation into ImageService
    Co-authored-by: Nickie Almind <nalm@itu.dk>

--- Annotations.API/Services/ImageService.cs
+++ Annotations.API/Services/ImageService.cs
@@ -1,10 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Annotations.API.Services;
 
+public record ValidationResponse(bool Success, string Message);
+
+
 public interface IImageService
+
 {
-    
+    ValidationResponse ValidateImage(IFormFile file);
 }
-public class ImageService
+
+public class ImageService: IImageService
 {
+    private static string[] _arrayOfFileExtension = {"png", "jpg", "jpeg"};
+    public ValidationResponse ValidateImage(IFormFile file)//temporary location
+    {
+        
+        if (file.Length > 50 * 1024 * 1024) {//50MB
+            return new ValidationResponse(false, "File is too large.");
+        }
+        if (file.Length == 0) 
+        { 
+            return new ValidationResponse(false, "File doesn't exist.");
+        }
+        foreach (string fileExtension in _arrayOfFileExtension)
+        {
+            if (file.ContentType.Contains(fileExtension))
+            {
+                //if the image is the correct type, it will be uploaded, since it fulfills the other criterias
+                //upload image to db
+                return new ValidationResponse(true ,"Uploaded image successfully.");
+            }
+        }
+        //if the code reaches this point, then the file type is none of the permitted file types, so an error is thrown
+        return new ValidationResponse(false, "File is not a valid image.");
+        
+    }
     
 }

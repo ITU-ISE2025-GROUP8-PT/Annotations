809251a4f92e893d9febc6474ab90cf2809ae9c2
tomat-suppe <slak@itu.dk>
2025-05-02 19:52:52 +0200

    Annotation entity, VesselObject.cs created. DBContext updated.
    This is only initial work, it is not finished.
    Inspiration has been taken from older branch,
    therefore co-authorship.
    
    Co-authored-by: Jakob Sønder <jakso@itu.dk>
    Co-authored-by: Ronas Olsen <rono@itu.dk>
    Co-authored-by: Nickie Almind <nalm@itu.dk>
    Co-authored-by: Ida Stougaard <iast@itu.dk>
    Co-authored-by: Jacob Sponholtz <spon@itu.dk>

--- Annotations.API/AnnotationsDbContext.cs
+++ Annotations.API/AnnotationsDbContext.cs
@@ -4,29 +4,55 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Annotations.API;
 
+/// <summary>
+/// Database context for the annotations application backend.
+/// </summary>
 public class AnnotationsDbContext : IdentityDbContext<User>
 {
-    public DbSet<User> Userz { get; set; } // name Users causes issues. Userz is a temporary name
-    public DbSet<Admin> Admins { get; set; }
-    public DbSet<MedicalProfessional> MedicalProfessionals { get; set; }
-    public DbSet<Image> Images { get; set; }
-    public DbSet<Dataset> Datasets { get; set; }
+    /// <summary>
+    /// Database set of users. Currently unused.
+    /// </summary>
+    public DbSet<User> Users { get; set; }
 
+    /// <summary>
+    /// Database set of images for research use.
+    /// </summary>
+    public DbSet<Image> Images { get; set; }
+    
+    /// <summary>
+    /// Database set of datasets
+    /// </summary>
+    public DbSet<Dataset> Datasets { get; set; }
+    
+    /// <summary>
+    /// Database set of annotations upon images
+    /// </summary>
+    public DbSet<Annotation> Annotation { get; set; }
+    
     public AnnotationsDbContext(DbContextOptions<AnnotationsDbContext> options) : base(options)
     {
-        Userz = Set<User>();
-        Admins = Set<Admin>();
-        MedicalProfessionals = Set<MedicalProfessional>();
+        Users = Set<User>();
         Images = Set<Image>();
         Datasets = Set<Dataset>();
+        Annotation = Set<Annotation>();
+        
     }
 
-    /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
 	{
-		base.OnModelCreating(modelBuilder);
+		base.OnModelCreating(builder);
+		
+		//Foreign key setup between AnnotationType and UserId
+		builder.Entity<Annotation>()
+			.HasOne<User>()
+			.WithMany()
+			.HasForeignKey("MpId");
 
-        modelBuilder.Entity<Dataset>().HasMany(d => d.ImageIds).WithMany().HasForeignKey(i => i.ImageId);
+		//Foreign key setup between AnnotationType and ImageId
+		builder.Entity<Annotation>()
+			.HasOne<Image>()
+			.WithMany()
+			.HasForeignKey("ImgId");
+		
 	}
-    */
-
 }
---
+++ Annotations.Core/Entities/Annotation.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Annotations.Core.VesselObjects;
+
+namespace Annotations.Core.Entities;
+
+public class Annotation
+{
+    [Required]
+    public required int Id { get; set; }
+
+    [ForeignKey("User")]
+    [Required]
+    public required int UserId { get; set; } //Id of the user, that made the Annotation
+    
+    [ForeignKey("Image")]
+    [Required]
+    public required int ImgId { get; set; } //Id of annotated image
+    
+    [Required]
+    public required VesselAnnotation AnnotationTree { get; set; }
+}

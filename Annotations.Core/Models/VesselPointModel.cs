db05c4b9ddd32e91390c058098b48e8f78ddb09d
tomat-suppe <slak@itu.dk>
2025-05-02 22:49:22 +0200

    Some thoughts on implementation in AnnotationsGroup.
    Added Model types for the new entity types

---
+++ Annotations.Core/Models/VesselPointModel.cs
@@ -0,0 +1,14 @@
+namespace Annotations.Core.Models;
+
+public class VesselPointModel
+{
+    public required int Id { get; set; }
+    
+    public required int X { get; set; } 
+    
+    public required int Y { get; set; }
+    
+    public required string Text { get; set; }
+    
+    public required string Type { get; set; }
+}
---
+++ Annotations.Core/Models/VesselSegmentModel.cs
@@ -0,0 +1,16 @@
+namespace Annotations.Core.Models;
+
+public class VesselSegmentModel
+{
+    public required int Id { get; set; }
+    
+    public required VesselPointModel StartPoint { get; set; }
+    
+    public required VesselPointModel EndPoint { get; set; }
+    
+    public required string Text { get; set; }
+    
+    public required double Thickness { get; set; }
+    
+    public required string Type { get; set; }
+}

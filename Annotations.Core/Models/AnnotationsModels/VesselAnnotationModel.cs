99adbfce5fcf2f39cfe2e50ec2394931dbc96439
tomat-suppe <slak@itu.dk>
2025-05-04 17:18:12 +0200

    Added model classes for remaining Annotatiom models.
    Moved all Annotation entities and models to dedicated directories

---
+++ Annotations.Core/Models/AnnotationsModels/VesselAnnotationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Annotations.Core.Models;
+
+public class VesselAnnotationModel
+{
+    public required int Id { get; set; }
+    
+    public required List<VesselPointModel> Points { get; set; }
+    
+    public required List<VesselSegmentModel> Segments { get; set; }
+}

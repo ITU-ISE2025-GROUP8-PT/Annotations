001b92afb7ef6492e49b5f95a49cba6d8f35aeb3
Jakob <jakso@itu.dk>
2025-05-07 14:48:01 +0200

    Fixed some playwright tests

--- Annotations.Blazor.Tests/E2E/RoleBasedAccessControlPlaywrightTests.cs
+++ Annotations.Blazor.Tests/E2E/RoleBasedAccessControlPlaywrightTests.cs
@@ -20,23 +20,23 @@ public class RoleBasedAccessControlPlaywrightTests : PageTest
     public async Task AccessDeniedDatasets()
     {
         await Page.GotoAsync("https://localhost:7238/images/datasets/");
-        await Page.GetByRole(AriaRole.Heading, new() { Name = "You are not welcome here!" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Img, new() { Name = "Access Denied" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Unfortunately you do not have the required permission to access this site." })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Img, new() { Name = "Access Denied" })).ToBeVisibleAsync();
     }    
     
     [Fact]
     public async Task AccessDeniedImageAnnotations()
     {
         await Page.GotoAsync("https://localhost:7238/images/annotations");
-        await Page.GetByRole(AriaRole.Heading, new() { Name = "You are not welcome here!" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Img, new() { Name = "Access Denied" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Unfortunately you do not have the required permission to access this site." })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Img, new() { Name = "Access Denied" })).ToBeVisibleAsync();
     }    
     
     [Fact]
     public async Task AccessDeniedApiAccess()
     {
         await Page.GotoAsync("https://localhost:7238/ApiAccess");
-        await Page.GetByRole(AriaRole.Heading, new() { Name = "You are not welcome here!" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Img, new() { Name = "Access Denied" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Unfortunately you do not have the required permission to access this site." })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Img, new() { Name = "Access Denied" })).ToBeVisibleAsync();
     }
 }

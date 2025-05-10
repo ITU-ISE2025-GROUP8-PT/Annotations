a6476ef1531977881ebeb26dfa44e98d579b9bc6
Ronas Olsen <rono@itu.dk>
2025-05-10 17:44:52 +0200

    Refactored mock store for use in more tests.

--- Annotations.API.Tests/ImageUploaderTests.cs
+++ Annotations.API.Tests/ImageUploaderTests.cs
@@ -20,13 +20,17 @@ public class ImageUploaderTests
 {
     /// <summary>
     /// Test to see that it is possible to upload an image to the database.
+    /// Initially, the database is created in memory and populated with 3 Image entries.
+    /// This means that the next image will be assigned ID 4.
     /// 
     /// Refer to Andrew Lock page 909 for more information on dbContext and in memory database.
     /// </summary>
     [Fact]
     public async Task CanUploadImage()
     {
-        // Arrange database context
+        // Arrange
+        
+        // 1. Database context
         var connection = new SqliteConnection("DataSource=:memory:");
         connection.Open();
 
@@ -50,29 +54,11 @@ public class ImageUploaderTests
             context.SaveChanges();
         }
 
-
-
-        // Arrange mock Azure Storage
-        var mockBlobServiceClientFactory = new Mock<IAzureClientFactory<BlobServiceClient>>();
-        var mockBlobServiceClient = new Mock<BlobServiceClient>();
-        var mockBlobContainerClient = new Mock<BlobContainerClient>();
-        var mockBlobClient = new Mock<BlobClient>();
-
-        mockBlobServiceClientFactory
-            .Setup(x => x.CreateClient(It.Is<string>(s => s.Equals("Default"))))
-            .Returns(mockBlobServiceClient.Object);
-
-        mockBlobServiceClient.
-            Setup(x => x.GetBlobContainerClient(It.Is<string>(s => s.Equals("images"))))
-            .Returns(mockBlobContainerClient.Object);
-
-        mockBlobContainerClient
-            .Setup(x => x.GetBlobClient(It.Is<string>(s => s.Equals("4"))))
-            .Returns(mockBlobClient.Object);
-
-        mockBlobClient
-            .Setup(x => x.Exists(default))
-            .Returns(Azure.Response.FromValue(false, Mock.Of<Azure.Response>()));
+        // 2. Mock Azure Storage
+        var mockStore = new MockAzureBlobStorageClientFactory(
+            expectedName: "Default",
+            expectedContainerName: "images",
+            expectedBlobName: "4");
 
 
 
@@ -81,7 +67,7 @@ public class ImageUploaderTests
 
         using (var context = new AnnotationsDbContext(options))
         {
-            var imageUploader = new ImageUploader(context, mockBlobServiceClientFactory.Object);
+            var imageUploader = new ImageUploader(context, mockStore.MockBlobServiceClientFactory.Object);
             imageUploader.OriginalFilename = "test.jpg";
             imageUploader.ContentType = "image/jpeg";
             imageUploader.InputStream = new MemoryStream(new byte[] { 0x01, 0x02, 0x03 });
@@ -96,5 +82,41 @@ public class ImageUploaderTests
         Assert.Equal(201, uploadResult.StatusCode);
         Assert.Equal(4, uploadResult.ImageId);
         Assert.Equal(string.Empty, uploadResult.Error);
+    }
+}
+
+
+
+/// <summary>
+/// Class to arrange the mock Azure Storage client factory.
+/// </summary>
+sealed class MockAzureBlobStorageClientFactory
+{
+    public Mock<IAzureClientFactory<BlobServiceClient>> MockBlobServiceClientFactory { get; } = new();
+
+    public MockAzureBlobStorageClientFactory(
+        string expectedName, 
+        string expectedContainerName,
+        string expectedBlobName)
+    {
+        var mockBlobServiceClient = new Mock<BlobServiceClient>();
+        var mockBlobContainerClient = new Mock<BlobContainerClient>();
+        var mockBlobClient = new Mock<BlobClient>();
+
+        MockBlobServiceClientFactory
+            .Setup(x => x.CreateClient(It.Is<string>(s => s.Equals(expectedName))))
+            .Returns(mockBlobServiceClient.Object);
+
+        mockBlobServiceClient.
+            Setup(x => x.GetBlobContainerClient(It.Is<string>(s => s.Equals(expectedContainerName))))
+            .Returns(mockBlobContainerClient.Object);
+
+        mockBlobContainerClient
+            .Setup(x => x.GetBlobClient(It.Is<string>(s => s.Equals(expectedBlobName))))
+            .Returns(mockBlobClient.Object);
+
+        mockBlobClient
+            .Setup(x => x.Exists(default))
+            .Returns(Azure.Response.FromValue(false, Mock.Of<Azure.Response>()));
     }
 }

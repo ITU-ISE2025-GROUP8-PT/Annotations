b4eb9810e57aa605c64aea1f7e5af7b533a3ae70
Ronas Olsen <rono@itu.dk>
2025-05-13 09:02:37 +0200

    Basic client side testing service.

---
+++ Annotations.Blazor.Client/Services/TestingService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+
+namespace Annotations.Blazor.Client.Services;
+
+
+/// <summary>
+/// Defines the contract for the testing service.
+/// </summary>
+public interface ITestingService
+{
+    Task<IEnumerable<string>> TryMeAsync();
+}
+
+
+
+/// <summary>
+/// This service is used to test the API.
+/// </summary>
+internal sealed class TestingService(HttpClient httpClient) : ITestingService
+{
+    public async Task<IEnumerable<string>> TryMeAsync() =>
+        await httpClient.GetFromJsonAsync<string[]>("api/testing/tryme") ?? ["Could not fetch content from \"api/testing/tryme\""];
+}

46a943accda24c000953c885ebd41ab867e458d1
Ronas Olsen <rono@itu.dk>
2025-05-13 16:00:43 +0200

    Added an image count.
    This was done to ensure that the count is available in constant time.

---
+++ Annotations.API/Migrations/20250513135858_ImageCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Annotations.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ImageCount",
+                table: "Datasets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageCount",
+                table: "Datasets");
+        }
+    }
+}

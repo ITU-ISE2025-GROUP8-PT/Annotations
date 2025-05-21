57c9a4b93db392717ebfb7b9fd6f9bb34a8bc2a4
iast <iast@itu.dk>
2025-05-21 12:55:18 +0200

    Added playwright tests for viewing user info. Deleted a Bunit test that was no longer needed.

--- Annotations.Blazor.Tests/Unit/PersonalHomeTests.cs
+++ Annotations.Blazor.Tests/Unit/PersonalHomeTests.cs
@@ -17,19 +17,5 @@ public class PersonalHomeTests : TestContext
         homePage.Find("h1").MarkupMatches("<h1>Annotations - PerfusionTech</h1>");
         homePage.Find("p").MarkupMatches("<p> Log in or make a new account to get access</p>");
     }
-    
-    [Fact]
-    public void AuthorizedTest()
-    {
-        // Arrange
-        JSInterop.Mode = JSRuntimeMode.Loose; // allows JS to run during bUnit tests
-        var authContext = this.AddTestAuthorization();
-        authContext.SetAuthorized("FAKE TEST USER"); // fakes authorized state for a fake user
-        
-        // Act
-        var homePage = RenderComponent<Home.Home>();
-        
-        // Assert
-        homePage.Find("h2").MarkupMatches("<h2> Welcome user</h2>");
-    }
+
 }
